8e24a7bbcee652968a82985a0431d0c4d7681a70
Eagudomsin <77778837+Eagudomsin@users.noreply.github.com>
2021-02-21 18:14:56 +0700

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,51 @@
+var spotlightVideo = document.getElementById("spotlightVideo");
+let card2 = document.getElementById("card2");
+let card3 = document.getElementById("card3");
+let card4 = document.getElementById("card4");
+let card5 = document.getElementById("card5");
+let card6 = document.getElementById("card6");
+let card7 = document.getElementById("card7");
+let card8 = document.getElementById("card8");
+let card9 = document.getElementById("card9");
+let card10 = document.getElementById("card10");
+let card11 = document.getElementById("card11");
+
+card2.addEventListener("click" , function videoCall1() {
+    spotlightVideo.src = "https://www.youtube.com/embed/Pj-aFpSe3U4";
+});
+
+card3.addEventListener("click" , function videoCall2() {
+    spotlightVideo.src = "https://www.youtube.com/embed/Y_GPQgHxwyk";
+});
+
+card4.addEventListener("click" , function videoCall3() {
+    spotlightVideo.src = "https://www.youtube.com/embed/vDYjzeMs1Bw";
+});
+
+card5.addEventListener("click" , function videoCall4() {
+    spotlightVideo.src = "https://www.youtube.com/embed/ZbKE2kh5QJg";
+});
+
+card6.addEventListener("click" , function videoCall5() {
+    spotlightVideo.src = "https://www.youtube.com/embed/7OdgBO4Ye8I";
+});
+
+card7.addEventListener("click" , function videoCall6() {
+    spotlightVideo.src = "https://www.youtube.com/embed/LqtkfCLjJz4";
+});
+
+card8.addEventListener("click" , function videoCall7() {
+    spotlightVideo.src = "https://www.youtube.com/embed/Wsz9FLRR7d8";
+});
+
+card9.addEventListener("click" , function videoCall8() {
+    spotlightVideo.src = "https://www.youtube.com/embed/FaNdtOLRaJQ";
+});
+
+card10.addEventListener("click" , function videoCall9() {
+    spotlightVideo.src = "https://www.youtube.com/embed/jjbCRUOLZXA";
+});
+
+card11.addEventListener("click" , function videoCall10() {
+    spotlightVideo.src = "https://www.youtube.com/embed/f7EhXv6NLys";
+});
